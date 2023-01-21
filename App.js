eaecb1634e5b7feb9e0aef69bfef97310d45a74b
ers1Fal <119732061+ers1Fal@users.noreply.github.com>
2023-01-21 15:02:25 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <div className="lenta">
+    <div className="news">
+    <li><a href="index6.html">Новости</a>
+        <ul> 
+        <li><a href="https://otkritkis.com/wp-content/uploads/2021/12/829efce4a920b208e39069728afe2d46-funny-caricatures-celebrity-caricatures.jpg">ФотограИИ</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Рекомендации</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">поиск</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Реакции</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">обновления</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Видео</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Друзья</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Подкасты</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Мои Подписки</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+     <li><a href="index6.html">Обновленмия</a>
+        <ul> 
+        <li><a href="что-то">что-то</a></li> 
+       </ul> 
+     </li> 
+    
+     
+                 
+    </div>
+  </div>
+    
+  );
+}
+
+export default App;
